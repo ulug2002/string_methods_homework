27c90d95f1edb516f3d832ea8e71983d09f43912
ulug2002 <ulugbek_it@mail.ru>
2023-03-15 16:27:42 +0300

    done1

--- String01.py
+++ String01.py
@@ -1,13 +1,13 @@
 def main(s):
-    """
+     """
     A str of several words is given. All letters are lowercase. Make sure that the first letter of each word is capitalized.
     Args:
         s: str
     Returns:
         str: answer
-    """
-    
-    return s()
-s = 'asdafa'.upper
+     """
+     return s()
+s = "sdfghj".upper
 print(main(s))
+
 
